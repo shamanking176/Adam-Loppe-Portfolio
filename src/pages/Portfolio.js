8f74cf310fc373b8950d12abda91cc2b162bf858
Adam <adamloppe6@gmail.com>
2023-06-20 16:47:03 -0400

    fixing portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,9 @@
 import '../../src/style.css'
+import petShelter from '../1683507154448.png'
+import techBlog from '../tech-blogs-1.jpg'
+import weather from '../FocusArea__Weather-02.jpg'
+import movie from '../online-cinema-art-movie-watching-260nw-586719869.webp'
+import readme from '../open-graph-logo.png'
 function Portfolio (){
     return (
         <section>
@@ -6,7 +11,22 @@ function Portfolio (){
                 Portfolio
             </h3>
             <a href="https://github.com/thossain30/Project-Pet-Shelter">
-      <img src='../background-design-poly-vektor-abstraktsiia-oka-project-golub.jpg' alt="Example 1" />
+      <img src={petShelter} alt="Pet Shelter"  width="250px" height="250px"/>
+    </a>
+    <a href="https://github.com/shamanking176/Tech-Blog">
+      <img src={techBlog} alt="Tech Blog"  width="250px" height="250px"/>
+    </a>
+    <a href="https://github.com/shamanking176/Tech-Blog">
+      <img src={weather} alt="Weather"  width="250px" height="250px"/>
+    </a>
+    <a href="https://github.com/shamanking176/Tech-Blog">
+      <img src={movie} alt="Movie"  width="250px" height="250px"/>
+    </a>
+    <a href="https://github.com/shamanking176/Tech-Blog">
+      <img src={readme} alt="README"  width="250px" height="250px"/>
+    </a>
+    <a href="https://github.com/shamanking176/Tech-Blog">
+      <img src={weather} alt="Tech Blog"  width="250px" height="250px"/>
     </a>
         </section>
     )
